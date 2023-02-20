9949b335fd4404d9a59ac4585c7bf964b83ecf19
Nelson <mungainelle96@gmail.com>
2023-02-20 13:43:54 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+let age=20
+let isAdult,canWork,canDrink,canEnlist
+switch(true){
+    case age>=21:
+        canDrink=true;
+    case age>=18:
+        isAdult=true;
+        canEnlist=true;
+    case age>=16:
+        canWork=true;
+}
+console.log(isAdult)
+console.log(canDrink)
+console.log(canWork)
